96399cae8efa72d7705f852539ae2fc4eaca22c2
Ben Gottlieb <ben@standalone.com>
2020-07-16 06:42:58 -0500

    updated Suite requirements

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["AudioEngine"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/bengottlieb/Suite.git", from: "0.9.51"),
+		.package(url: "https://github.com/bengottlieb/Suite.git", from: "0.9.52"),
 
     ],
     targets: [
