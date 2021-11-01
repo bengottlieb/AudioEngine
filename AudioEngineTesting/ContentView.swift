7149d3b84dfe46493bf8336e099dbe7dbff60351
Ben Gottlieb <ben@standalone.com>
2021-11-01 05:45:18 -0500

    cleaned up test project

--- AudioEngineTesting/ContentView.swift
+++ AudioEngineTesting/ContentView.swift
@@ -13,26 +13,57 @@ class FileList: ObservableObject {
 	
 	init() {
 		let directory = URL.document(named: "files")
+		try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
 		if let sample = URL.bundled(named: "sample.mp3") {
 			try? FileManager.default.copyItem(at: sample, to: directory.appendingPathComponent("sample.mp3"))
 		}
-		
-		self.files = try! FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil, options: [])
+
+		if let sample = URL.bundled(named: "piano-cassical-10sec.mp3") {
+			try? FileManager.default.copyItem(at: sample, to: directory.appendingPathComponent("piano-cassical-10sec.mp3"))
+		}
+
+		do {
+			self.files = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil, options: [])
+		} catch {
+			self.files = []
+		}
 	}
 }
 
 struct ContentView: View {
 	@StateObject var files = FileList()
 	@ObservedObject var channel = AudioMixer.instance.mainChannel
+	@State var current: URL?
+	
+	func pause() {
+		channel.pause(outro: .linearFade(2))
+		current = nil
+	}
 	
 	func play(_ url: URL) {
 		guard let track = AudioTrack(url: url) else {
-			logg("Failed to build track for \(url)")
+			Studio.logg("Failed to build track for \(url)")
 			return
 		}
 		
+		current = url
+		
 //		channel.enqueue(track: track)
 		try! channel.play(track: track)
+	}
+	
+	func loop(_ url: URL) {
+		guard let track = AudioTrack(url: url) else {
+			Studio.logg("Failed to build track for \(url)")
+			return
+		}
+		
+		current = url
+//		channel.enqueue(track: track)
+		
+		let queue = AudioQueue(tracks: [track], intro: nil, outro: nil, useLoops: true)
+		channel.setQueue(queue)
+		try? channel.play()
 	}
 	
 	var body: some View {
@@ -42,13 +73,29 @@ struct ContentView: View {
 		}
 		
 		List(files.files) { url in
-			Button(action: { play(url) }) {
-				HStack() {
-					Text(url.lastPathComponent)
-					Image(.play)
+			HStack() {
+				Text(url.lastPathComponent)
+				Spacer()
+				if current == url {
+					Button(action: { pause() }) {
+						Image(.pause)
+							.padding(.horizontal)
+					}
+					.buttonStyle(PlainButtonStyle())
+				} else {
+					Button(action: { play(url) }) {
+						Image(.play)
+							.padding(.horizontal)
+					}
+					.buttonStyle(PlainButtonStyle())
+
+					Button(action: { loop(url) }) {
+						Image(.arrow_clockwise)
+							.padding(.horizontal)
+					}
+					.buttonStyle(PlainButtonStyle())
 				}
 			}
-			.buttonStyle(PlainButtonStyle())
 		}
 	}
 }
